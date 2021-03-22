79b1490fae55ececf531d8c148ff96b79e78c758
qiubaisen <abosen@qq.com>
2021-03-22 10:50:23 +0800

    Initial commit

---
+++ src/main/java/com/github/qbosen/sortabledemo/Application.java
@@ -0,0 +1,16 @@
+package com.github.qbosen.sortabledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiubaisen
+ * @date 2021/3/22
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
