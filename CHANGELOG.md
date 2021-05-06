103d3e840d38f0b3bf677646295fa6af9e7cf1e7
qiubaisen <abosen@qq.com>
2021-05-06 16:48:55 +0800

    feat: add NonZero validation
    增加非0的校验工具
    支持 Integer/Short/Long/Float/Double/Byte/BigInteger/BigDecimal 数据类型
    
    RELEASE 0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres
+to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+```text
+Added 新添加的功能。
+Changed 对现有功能的变更。
+Deprecated 已经不建议使用，准备很快移除的功能。
+Removed 已经移除的功能。
+Fixed 对bug的修复
+Security 对安全的改进
+```
+
+## [Unreleased]
+
+## [0.0.2]
+
+- NonZero的validator工具
+
+## [0.0.1]
+
+### Added
+
+- 分页查询相关工具类和数据结构
+- 基础的异常定义
+- mybatisPlus相关增强支持
+- validator工具
---
+++ src/main/java/top/abosen/dddboot/spring/validator/NonZero.java
@@ -0,0 +1,84 @@
+package top.abosen.dddboot.spring.validator;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author qiubaisen
+ * @date 2021/5/6
+ */
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = {
+        NonZero.NotZeroIntegerValidator.class,
+        NonZero.NotZeroLongValidator.class,
+        NonZero.NotZeroShortValidator.class,
+        NonZero.NotZeroFloatValidator.class,
+        NonZero.NotZeroDoubleValidator.class,
+        NonZero.NotZeroByteValidator.class,
+        NonZero.NotZeroBigIntegerValidator.class,
+        NonZero.NotZeroBigDecimalValidator.class,
+})
+public @interface NonZero {
+    String message() default "must not zero";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    class NotZeroIntegerValidator implements ConstraintValidator<NonZero, Integer> {
+        @Override public boolean isValid(Integer num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+    class NotZeroLongValidator implements ConstraintValidator<NonZero, Long> {
+        @Override public boolean isValid(Long num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+    class NotZeroShortValidator implements ConstraintValidator<NonZero, Short> {
+        @Override public boolean isValid(Short num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+    class NotZeroFloatValidator implements ConstraintValidator<NonZero, Float> {
+        @Override public boolean isValid(Float num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+    class NotZeroDoubleValidator implements ConstraintValidator<NonZero, Double> {
+        @Override public boolean isValid(Double num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+    class NotZeroByteValidator implements ConstraintValidator<NonZero, Byte> {
+        @Override public boolean isValid(Byte num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+
+    class NotZeroBigIntegerValidator implements ConstraintValidator<NonZero, BigInteger> {
+        @Override public boolean isValid(BigInteger num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+
+    class NotZeroBigDecimalValidator implements ConstraintValidator<NonZero, BigDecimal> {
+        @Override public boolean isValid(BigDecimal num, ConstraintValidatorContext constraintValidatorContext) {
+            return num == null || NumberSignHelper.signum(num) != 0;
+        }
+    }
+}
