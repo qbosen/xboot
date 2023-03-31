ebbc18dd62140956d6fe19dc0a52289b59cdb321
qiubaisen <abosen@qq.com>
2023-03-31 08:39:11 +0800

    refactor(broadcast): refactor spring boot test

--- broadcast-message/broadcast-message-starter-redis/src/test/java/top/abosen/xboot/broadcast/redis/ApplicationTest.java
+++ broadcast-message/broadcast-message-starter-redis/src/test/java/top/abosen/xboot/broadcast/redis/ApplicationTest.java
@@ -1,15 +1,8 @@
 package top.abosen.xboot.broadcast.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 import java.util.UUID;
 
@@ -21,37 +14,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException
  * @since 2023/3/29
  */
 
-@SpringBootTest(classes = {ApplicationTest.MockConfig.class/*note: 先于autoconfiguration */, BroadcastRedisAutoConfiguration.class},
+@SpringBootTest(classes = {SprintBootTestConfiguration.class/*note: 先于autoconfiguration */, BroadcastRedisAutoConfiguration.class},
         webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = "xboot.broadcast.redis.topic=foo")
 public class ApplicationTest {
-    @Configuration(proxyBeanMethods = false)
-    public static class MockConfig {
-        @Bean
-        RedisConnectionFactory connectionFactory() {
-            return Mockito.mock(RedisConnectionFactory.class);
-        }
-
-        @Bean
-        StringRedisTemplate stringRedisTemplate() {
-            return Mockito.mock(StringRedisTemplate.class);
-        }
-
-        @Bean(name = BroadcastRedisAutoConfiguration.REDIS_BROADCAST_MESSAGE_LISTENER_CONTAINER)
-        RedisMessageListenerContainer listenerContainer() {
-            return Mockito.mock(RedisMessageListenerContainer.class);
-        }
-
-        @Bean
-        ObjectMapper objectMapper() {
-            return new ObjectMapper();
-        }
-    }
 
     @Autowired
     BroadcastRedisProperties properties;
 
     @Test
-    void should() {
+    void should_configure_instanceId_as_uuid_by_default() {
         assertThat(properties.getTopic()).isEqualTo("foo");
         assertThat(properties.getInstanceId()).isNotBlank();
         assertThatNoException().isThrownBy(() -> {
